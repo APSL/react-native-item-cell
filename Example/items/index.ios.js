c03e62384fdb019b583526a54558e021ce248aa0
alvaromb <amedina@apsl.net>
2015-09-03 16:40:00 +0200

    Added example

---
+++ Example/items/index.ios.js
@@ -0,0 +1,57 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ */
+'use strict';
+
+var React = require('react-native');
+var {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  ListView,
+} = React;
+import ItemCell from 'react-native-item-cell';
+
+var items = React.createClass({
+  getInitialState: function() {
+    var items = [
+      'Item 1',
+      'Item 2',
+      'Item 3',
+      'Settings'
+    ];
+    var dataSource = new ListView.DataSource({
+      rowHasChanged: (r1, r2) => r1 !== r2
+    });
+    return {
+      ds: dataSource.cloneWithRows(items)
+    };
+  },
+  _renderRow: function(rowData) {
+    return (
+      <ItemCell>{rowData}</ItemCell>
+    );
+  },
+  render: function() {
+    return (
+      <ListView style={styles.container}
+        contentInset={{top: 0}}
+        automaticallyAdjustContentInsets={false}
+        dataSource={this.state.ds}
+        renderRow={(rowData) => this._renderRow(rowData)}
+      />
+    );
+  }
+});
+
+var styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#F5FCFF',
+    marginTop: 14,
+  },
+});
+
+AppRegistry.registerComponent('items', () => items);
