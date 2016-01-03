8bb9e03442e5c5fcecaabea9d8381c4748be8109
Fabrice Armisen <farmisen@g mail.com>
2016-01-02 19:04:49 -0800

    Adding extra styling props.

--- ItemCell.js
+++ ItemCell.js
@@ -4,24 +4,47 @@ import Icon from 'react-native-vector-icons/FontAwesome'
 class ItemCell extends Component {
   _renderDisclosureIndicator () {
     if (this.props.showDisclosureIndicator) {
-      return <Icon style={styles.chevron} name='angle-right' size={22} />
+      return <Icon style={this._styles.chevron} name='angle-right' size={22} />
     }
   }
 
   _renderIcon () {
     if (this.props.icon) {
       return (
-        <View style={styles.iconContainer}>
-          <View style={styles.paddingView} />
-          <Image style={styles.icon}
-            source={{uri: this.props.icon}}
-            resizeMode='cover'
+        <View style={this._styles.iconContainer}>
+          <View style={this._styles.paddingView} />
+          <Image style={this._styles.icon}
+           source={{uri: this.props.icon}}
+           resizeMode='cover'
           />
-          <View style={styles.paddingView} />
+          <View style={this._styles.paddingView} />
         </View>
       )
     }
-    return <View style={styles.paddingView} />
+    return <View style={this._styles.paddingView} />
+  }
+
+  _updateStyle (props) {
+    this._styles = {
+      container: [styles.container, {backgroundColor: this.props.backgroundColor || 'white'}],
+      viewContainer: [styles.viewContainer, {backgroundColor: this.props.backgroundColor || 'white'}],
+      text: [styles.text, {backgroundColor: this.props.backgroundColor || 'white',color: this.props.textColor || '#000000'}],
+      textContainer: [styles.textContainer, {backgroundColor: this.props.backgroundColor || 'white'}],
+      bottomBorder: [styles.bottomBorder, {borderBottomColor: this.props.separatorColor || '#C8C7CC'}],
+      paddingView: [styles.paddingView, {backgroundColor: this.props.backgroundColor || 'white'}],
+      iconContainer: [styles.iconContainer, {backgroundColor: this.props.backgroundColor || 'white'}],
+      icon: [styles.icon, {backgroundColor: this.props.iconBackgroundColor || 'white'}],
+      chevron: [styles.chevron, {backgroundColor: this.props.backgroundColor || 'white',color: this.props.chevronColor || '#C8C7CC'}]
+    }
+  }
+
+  constructor (props) {
+    super(props);
+    this._updateStyle(props)
+  }
+
+  componentWillReceiveProps (nextProps) {
+    this._updateStyle(nextProps)
   }
 
   render () {
@@ -38,14 +61,14 @@ class ItemCell extends Component {
     return (
       <TouchableHighlight {...touchableProps}
         underlayColor='#D9D9D9'
-        style={styles.container}>
-        <View style={styles.viewContainer}>
-          <View style={styles.leftContainer}>
+        style={this._styles.container}>
+        <View style={this._styles.viewContainer}>
+          <View style={this._styles.leftContainer}>
             {this._renderIcon()}
           </View>
-          <View style={styles.bottomBorder}>
-            <View style={styles.textContainer}>
-              <Text style={styles.text}>
+          <View style={this._styles.bottomBorder}>
+            <View style={this._styles.textContainer}>
+              <Text style={this._styles.text}>
                 {this.props.children}
               </Text>
               {this._renderDisclosureIndicator()}
@@ -62,6 +85,11 @@ ItemCell.propTypes = {
   children: PropTypes.string.isRequired,
   showDisclosureIndicator: PropTypes.bool,
   icon: PropTypes.string,
+  backgroundColor: PropTypes.string,
+  separatorColor: PropTypes.string,
+  iconBackgroundColor: PropTypes.string,
+  chevronColor: PropTypes.string,
+  textColor: PropTypes.string,
 }
 
 const styles = StyleSheet.create({
