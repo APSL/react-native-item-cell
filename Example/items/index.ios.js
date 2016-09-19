41083b47d9e96d5fcfafa40d2525687b9fcef42d
alvaromb <amedina@apsl.net>
2016-09-19 16:16:47 +0200

    closes #11 Updated the Example

--- Example/items/index.ios.js
+++ Example/items/index.ios.js
@@ -1,40 +1,38 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- */
-'use strict';
+/* @flow */
 
-var React = require('react-native');
-var {
+import React from 'react'
+import {
   AppRegistry,
   StyleSheet,
   ListView,
-} = React;
-import ItemCell from 'react-native-item-cell';
+} from 'react-native'
+import ItemCell from 'react-native-item-cell'
 
-var items = React.createClass({
+const items = React.createClass({
   getInitialState: function() {
-    var itemsArray = [
+    const itemsArray = [
       'Item 1',
       'Item 2',
       'Item 3',
       'Settings',
       'It appears that the systematic use of complex symbols can be defined in such a way as to impose the extended c-command discussed in connection with (34).',
       'With this clarification, any associated supporting element is unspecified with respect to a corpus of utterance tokens upon which conformity has been defined by the paired utterance test.'
-    ];
-    var dataSource = new ListView.DataSource({
+    ]
+    const dataSource = new ListView.DataSource({
       rowHasChanged: (r1, r2) => r1 !== r2
-    });
+    })
     return {
       ds: dataSource.cloneWithRows(itemsArray)
-    };
+    }
   },
+
   _renderRow: function(rowData, sectionID, rowID, highlightRow) {
     if (rowID === '1') {
       return (
         <ItemCell
-          icon={{uri: 'http://sourcefreeze.com/wp-content/uploads/2015/04/react-native.png'}}>
-          URL Icon - {rowData}
+          subtitle='GraphQL is an amazing technology.'
+          icon={require('./graphql.png')}>
+          Local assets icon
         </ItemCell>
       )
     }
@@ -42,14 +40,21 @@ var items = React.createClass({
       return (
         <ItemCell
           icon={{uri: 'https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png'}}>
-          URL Icon - {rowData}
+          {`URL Icon - ${rowData}`}
         </ItemCell>
       )
     }
     if (rowID === '2') {
       return (
         <ItemCell showDisclosureIndicator={true} icon={require('./apsl.png')}>
-          Local image - {rowData}
+          Local image
+        </ItemCell>
+      )
+    }
+    if (rowID === '3') {
+      return (
+        <ItemCell icon={require('./rn.png')}>
+          React Native
         </ItemCell>
       )
     }
@@ -58,26 +63,28 @@ var items = React.createClass({
       <ItemCell showDisclosureIndicator={disclosure}>
         {rowData}
       </ItemCell>
-    );
+    )
   },
+
   render: function() {
     return (
-      <ListView style={styles.container}
+      <ListView
+        style={styles.container}
         contentInset={{top: 0}}
         automaticallyAdjustContentInsets={false}
         dataSource={this.state.ds}
-        renderRow={(rowData, sectionID, rowID, highlightRow) => this._renderRow(rowData, sectionID, rowID, highlightRow)}
+        renderRow={this._renderRow}
       />
-    );
+    )
   }
-});
+})
 
-var styles = StyleSheet.create({
+const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#F5FCFF',
-    marginTop: 14,
+    backgroundColor: '#efedf4',
+    paddingTop: 20,
   },
-});
+})
 
-AppRegistry.registerComponent('items', () => items);
+AppRegistry.registerComponent('items', () => items)
